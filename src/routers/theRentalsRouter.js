5ba636e43f7c83fe5eb20d896ed930541a9e1b0a
MarianaKely <marianakely.r@gmail.com>
2023-05-11 17:09:04 -0300

    rental completion route implemented

--- src/routers/theRentalsRouter.js
+++ src/routers/theRentalsRouter.js
@@ -1,12 +1,13 @@
 
 import { Router } from "express";
-import { allRentals , individualRents } from "../controllers/theRentals.js";
+import { allRentals , individualRents , RentalConfig } from "../controllers/theRentals.js";
 
 
 const theRentalsRouter = Router();
 
 theRentalsRouter.get("/rentals", allRentals);
 theRentalsRouter.post("/rentals", individualRents);
+theRentalsRouter.post("/rentals/:id/return", RentalConfig);
 
 
 export default theRentalsRouter;
