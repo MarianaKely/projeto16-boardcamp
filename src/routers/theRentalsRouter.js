c4637255ddf7ffbfc8353c9ec6a75f3a2b3cc71c
MarianaKely <marianakely.r@gmail.com>
2023-05-11 17:24:35 -0300

    delete reantal router implemented

--- src/routers/theRentalsRouter.js
+++ src/routers/theRentalsRouter.js
@@ -1,12 +1,13 @@
 
 import { Router } from "express";
-import { allRentals , individualRents , RentalConfig } from "../controllers/theRentals.js";
+import { allRentals , individualRents , erase , RentalConfig } from "../controllers/theRentals.js";
 
 
 const theRentalsRouter = Router();
 
 theRentalsRouter.get("/rentals", allRentals);
 theRentalsRouter.post("/rentals", individualRents);
+theRentalsRouter.delete("/rentals/:id", erase);
 theRentalsRouter.post("/rentals/:id/return", RentalConfig);
 
 
